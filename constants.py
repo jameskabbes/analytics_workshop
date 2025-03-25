3a10240d8d0386bdb6d9e94d96503ac3a57bf8f8
James Kabbes <james.kabbes@gmail.com>
2025-03-25 15:24:37 -0500

    adding new output file paths

--- constants.py
+++ constants.py
@@ -10,3 +10,6 @@ FEATURES_PATH = DATA_DIR / 'features.csv'
 TARGET_KNOWN_PATH = DATA_DIR / 'target_known.csv'
 TARGET_UNKNOWN_PATH = DATA_DIR / 'target_unknown.csv'
 SUBMISSION_PINS_PATH = DATA_DIR / 'submission_pins.csv'
+FORM_RESPONSES_PATH = DATA_DIR / \
+    '2025 - John Deere Analytics Workshop _ Psi Eta Mu (Responses) - Form Responses 1.csv'
+RESULTS_PATH = DATA_DIR / 'results.csv'
